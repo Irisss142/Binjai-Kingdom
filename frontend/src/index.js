244767f1e30751f480e86904b9feb2484e0845be
Iris <62509409+Irisss142@users.noreply.github.com>
2024-10-12 15:37:13 +0800

    s

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './styles/styles.css';
 import App from './App';
 
-ReactDOM.render(
-    <React.StrictMode>
-        <App
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
